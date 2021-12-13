6ecfa710cf15520d35118c0fd8a0a7e2be48978b
Nikita Dubovik <flashes.n.co@gmail.com>
2021-12-13 16:58:44 +0200

    Auth interim commit

---
+++ Posts.demo/Modules/Auth/AuthConfigurator.swift
@@ -0,0 +1,8 @@
+//
+//  AuthConfigurator.swift
+//  Posts.demo
+//
+//  Created by devmac on 13.12.2021.
+//
+
+import Foundation
---
+++ Posts.demo/Modules/Auth/AuthPresenter.swift
@@ -0,0 +1,8 @@
+//
+//  AuthPresenter.swift
+//  Posts.demo
+//
+//  Created by devmac on 13.12.2021.
+//
+
+import Foundation
